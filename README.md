fc4054a32af626bc6a890dca1475ff92bd315204
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-01-15 13:00:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_mlkit_sandbox
+
+flutter mlkit sandbox
---
+++ android/app/src/main/kotlin/kr/co/bullets/flutter_mlkit_sandbox/MainActivity.kt
@@ -0,0 +1,5 @@
+package kr.co.bullets.flutter_mlkit_sandbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
